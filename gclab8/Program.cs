69ad4f8948de2c48eaa1809ca339c9216ed67810
chris <butchchr@gmail.com>
2018-07-24 13:38:33 -0400

    first commit, names array

--- gclab8/Program.cs
+++ gclab8/Program.cs
@@ -10,6 +10,7 @@ namespace gclab8
     {
         static void Main(string[] args)
         {
+            string[] students ={"Simon Schneider", "Kyle Silva", "Santiago Martin", "Gustavo Mcgee", "Ron Keller", "Meghan Burke", "Florence Carr", "Kari Frank", "Madeline Montgomery", "Laurie Wilkins", "Heather Hicks", "Eddie Alvarado", "Tasha Hart", "Rodolfo Waters", "Bryan Howell", "Mae Martinez","Erica Collier", "Marcella Ramirez", "Valerie Blair"};
         }
     }
 }
